2da3934b6d47542c9feb20828867342bf010462c
Raulp01 <raul.pianon@outlook.com>
2023-05-18 10:22:37 +0200

    Aggiunto il plugin checkstyle in pom.xml. Configurate le verifiche nel file checkstyle.xml close #2

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,13 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// [RAUL] [PIANON] [2045040]
+// [LUCA] [LA ROTONDA] [2042784]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
