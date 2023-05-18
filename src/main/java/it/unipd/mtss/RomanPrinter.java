df7e11f20af1e73d64db01250cb0983a9781a437
Raulp01 <raul.pianon@outlook.com>
2023-05-18 11:28:52 +0200

    Primi 3 numeri romani con test e coveredratio di Jacoco impostato a 85

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,13 +5,42 @@
 
 package it.unipd.mtss;
 
+import java.util.HashMap;
+
 public class RomanPrinter {
     public static String print(int num) {
-        return null;
+        return printAsciiArt(IntegerToRoman.convert(num));
     }
 
     public static String printAsciiArt(String romanNumber) {
-        // TODO
-        return null;
+        if (romanNumber.isEmpty() || romanNumber.length() >= 13) {
+            throw new IllegalArgumentException();
+        }
+        HashMap<Character, String> romanHash = new HashMap<Character, String>();
+        String romanAscii = "";
+        for (int i = 0; i < 6; ++i) {
+            String romanLine = "";
+            int j = 0;
+            romanHash.put('I', LetterI(i));
+            while (j < romanNumber.length()) {
+                char roman_letter = romanNumber.charAt(j);
+                romanLine += romanHash.get(roman_letter);
+                j++;
+            }
+            romanAscii += romanLine + "\n";
+            romanHash.clear();
+        }
+        return romanAscii;
+    }
+
+    private static String LetterI(int i) {
+        String letterI[][] = { { " _____  " },
+                { "|_   _| " },
+                { "  | |   " },
+                { "  | |   " },
+                { " _| |_  " },
+                { "|_____| " } };
+        letterI[i][0].replace("\n", "");
+        return letterI[i][0];
     }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,12 +1,49 @@
 package it.unipd.mtss;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class RomanPrinterTest {
+
     @Test
-    public void setUpRomanPrinter() {
-        assertTrue(true);
+    public void firstThreeNumbers() {
+
+        String letter_I = " _____  \n" +
+                "|_   _| \n" +
+                "  | |   \n" +
+                "  | |   \n" +
+                " _| |_  \n" +
+                "|_____| \n";
+        String letter_II = " _____   _____  \n" +
+                "|_   _| |_   _| \n" +
+                "  | |     | |   \n" +
+                "  | |     | |   \n" +
+                " _| |_   _| |_  \n" +
+                "|_____| |_____| \n";
+        String letter_III = " _____   _____   _____  \n" +
+                "|_   _| |_   _| |_   _| \n" +
+                "  | |     | |     | |   \n" +
+                "  | |     | |     | |   \n" +
+                " _| |_   _| |_   _| |_  \n" +
+                "|_____| |_____| |_____| \n";
+
+        String value_I = RomanPrinter.print(1);
+        String value_II = RomanPrinter.print(2);
+        String value_III = RomanPrinter.print(3);
+
+        assertEquals(letter_I, value_I);
+        assertEquals(letter_II, value_II);
+        assertEquals(letter_III, value_III);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ReceiveEmptyString() {
+        RomanPrinter.printAsciiArt("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ReceiveTooLongString() {
+        RomanPrinter.printAsciiArt("MDCCCLXXXVIII");
     }
 }
